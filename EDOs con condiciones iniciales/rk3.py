8f08a17aec507dc81fc97e1ab3027e778ff9a375
Alvaro Siesquen <alvaro.siesquen@unmsm.edu.pe>
2023-11-27 00:47:29 -0500

    Actualizando metodos de RK en Python

--- EDOs con condiciones iniciales/rk3.py
+++ EDOs con condiciones iniciales/rk3.py
@@ -1,20 +1,21 @@
 # ------------------------------------------------------------------------
-# EDO con condicion inicial mediante el metodo de Runge-Kutta de 2do orden
+# EDO con condicion inicial mediante el metodo de Runge-Kutta de 3er orden
 # Autor: Alvaro Siesquen
 # ------------------------------------------------------------------------
 
 import numpy as np
 f = lambda y,t: np.exp(t)
 
-def met_rk2(t, y, tf, h, f):
+def met_rk3(t, y, tf, h, f):
     N = int((tf-t)/h)
     T, Y = [t], [y]
 
     for i in range(N):
         k1 = h*f(y,t)
-        k2 = h*f(y+k1,t+h)
+        k2 = h*f(y+k1/2,t+h/2)
+        k3 = h*f(y-k1+2*k2,t+h)
 
-        y = y + (k1 + k2)/2
+        y = y + (k1 + 4*k2 + k3)/6
         t = t + h
         
         Y.append(y)
